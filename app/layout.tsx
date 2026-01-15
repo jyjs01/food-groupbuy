1e7709c07a1eddf3d494447b4b0e3f631b3fca6f
JY <duawodud12@naver.com>
2026-01-15 22:34:05 +0900

    메타데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Providers from "@app/providers";
 export const metadata: Metadata = {
   title: "척척밥상 | 공동구매 상품 목록",
   description:
-    "랜덤 지연(1~5초) API 응답을 고려해 스켈레톤/재시도 UX를 적용하고, 품절은 최상단 고정 + 나머지는 index 오름차순으로 정렬해 표시합니다.",
+    "랜덤 지연(1~5초) API 응답을 고려해 로딩 UI를 적용하고, 품절은 최하단 고정 + 나머지는 index 오름차순으로 정렬해 표시합니다.",
 };
 
 export const viewport: Viewport = {
